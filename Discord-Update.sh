654c3ca68cb04376575f20264ce1b19d26a63e2f
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-16 09:39:18 +0200

    Update Discord-Update.sh
    Code optimization

--- Discord-Update.sh
+++ Discord-Update.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
+Discord-Install-Routine(){
+	sleep 1
+
+	# Create temporary directory
+	mkdir /var/discord
+
+	# Download and install latest version (.deb)
+  	echo -e "Download latest version of Discord."
+	wget -O /var/discord/discord-installer.deb "https://discord.com/api/download/stable?platform=linux&format=deb"
+	apt install /var/discord/discord-installer.deb
+	sleep 1
+	
+	# Clean up
+	rm -rf /var/discord/
+}
+
+
 echo -e "This script is used to easily and automatically update the Discord client. Unfortunately, it still does not have a repository. If Discord is not installed on the target system, this script will install Discord automatically."
 echo -e "Note: This script requires the wget and curl packages. If wget or curl are not installed, this script will install wget and curl!"
 
 # Check for root privileges
 if [ "$EUID" -ne 0 ]; then
-  echo "Error: This script requires root privileges. Please run with sudo."
-  exit 1
+	echo "Error: This script requires root privileges. Please run with sudo."
+	exit 1
 fi
 
 # Is wget installed?
 dpkg -s wget &> /dev/null
 if [ $? -eq 0 ]; then
-  echo -e "wget is installed! Dependencies complete!"
+	echo -e "wget is installed! Dependencies complete!"
 else
-    echo "The package wget is NOT installed! Wegt will be installed now!"
-    sleep 5
-    apt install wget
+	echo "The package wget is NOT installed! Wegt will be installed now!"
+	sleep 5
+	apt install wget
 fi
 
 # Is curl installed?
 dpkg -s curl &> /dev/null
 if [ $? -eq 0 ]; then
-  echo -e "curl is installed! Dependencies complete!"
+	echo -e "curl is installed! Dependencies complete!"
 else
-    echo "The package curl is NOT installed! Curl will be installed now!"
-    sleep 5
-    apt install curl
+	echo "The package curl is NOT installed! Curl will be installed now!"
+	sleep 5
+	apt install curl
 fi
 
 echo "All Dependencies complete!"
@@ -34,7 +51,6 @@ echo "All Dependencies complete!"
 # Is Discord installed?
 dpkg -s discord &> /dev/null
 if [ $? -eq 0 ]; then
-
 	# Get remote version and location
 	LocationString=$(curl --location --head 'https://discord.com/api/download/stable?platform=linux&format=deb' | grep "location:")
 	echo -e "The location of the latest version is: " $LocationString
@@ -46,53 +62,21 @@ if [ $? -eq 0 ]; then
 	VersionLocal=$(dpkg-query -f '${Version}' -W discord)
 	echo -e "Currently installed is: discord" $VersionLocal
 
-
 	# Check for a newer version
 	if test $VersionLocal != $VersionRemote; then echo "Discord $VersionRemote is newer than Discord $VersionLocal"; fi
 
 	if test $VersionLocal != $VersionRemote; then
- 
 		echo -e "Discord will be updated shortly..."
-		sleep 1
-
-		# Create temporary directory
-		mkdir /var/discord
-
-		# Download latest version (.deb)
-  		echo -e "Download Discord $VersionRemote"
-		wget -O /var/discord/discord-installer.deb "https://discord.com/api/download/stable?platform=linux&format=deb"
-
-		# Install latest version
-		apt install /var/discord/discord-installer.deb
-		echo -e "Discord is now up to date! Please wait for clean up..."
-		sleep 1
-
-		# Clean up
-		rm -rf /var/discord/
-
+		Discord-Install-Routine
+		echo -e "Discord is now up to date!"
 	else
 		# Discord can not be update, because the latest version is installed
     		echo "The package discord is already up to date!"
-
 	fi
-
 else
 	# Discord can not be update, because Discord is not installed
     	echo "The package discord is NOT installed! Discorad will be installed now!"
-
-	# Create temporary directory
-	mkdir /var/discord
-
-	# Download latest version (.deb)
-	wget -O /var/discord/discord-installer.deb "https://discord.com/api/download/stable?platform=linux&format=deb"
-
-	# Install latest version
-	apt install /var/discord/discord-installer.deb
-	sleep 1
-
-	# Clean up
-	rm -rf /var/discord/
-
+	Discord-Install-Routine
   	echo -e "Discord is installed!"
 fi
 
