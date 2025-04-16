091b513ea4b30485d76e53b02510b9ffb03d7c69
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-16 08:52:17 +0200

    Update Discord-Update.sh
    Added check for root privileges

--- Discord-Update.sh
+++ Discord-Update.sh
@@ -3,6 +3,12 @@
 echo -e "This script is used to easily and automatically update the Discord client. Unfortunately, it still does not have a repository. If Discord is not installed on the target system, this script will install Discord automatically."
 echo -e "Note: This script requires the wget and curl packages. If wget or curl are not installed, this script will install wget and curl!"
 
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Error: This script requires root privileges. Please run with sudo."
+  exit 1
+fi
+
 #is wget installed?
 dpkg -s wget &> /dev/null
 if [ $? -eq 0 ]; then
