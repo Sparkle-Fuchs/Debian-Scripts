f5d3f7bef901321bb1c6e37fac452531d4fd27d7
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-22 17:43:10 +0200

    Create Install-Spotify.sh
    https://www.spotify.com/uk/download/linux/

---
+++ Install-Spotify.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+	echo "Error: This script requires root privileges. Please run with sudo."
+	exit 1
+fi
+
+apt update && apt upgrade
+
+dpkg -s apt-transport-https &> /dev/null
+if [ $? -eq 0 ]; then
+	echo -e "apt-transport-https is installed! Dependencies complete!"
+else
+	echo "The package apt-transport-https is NOT installed! Curl will be installed now!"
+	sleep 5
+	apt install apt-transport-https
+fi
+
+dpkg -s curl &> /dev/null
+if [ $? -eq 0 ]; then
+	echo -e "curl is installed! Dependencies complete!"
+else
+	echo "The package curl is NOT installed! Curl will be installed now!"
+	sleep 5
+	apt install curl
+fi
+
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+apt update
+apt install spotify-client
+
+exit
