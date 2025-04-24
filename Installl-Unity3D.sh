6718227e258ecb820cbfbcf48c527da749ab7908
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-24 20:17:59 +0200

    Create Installl-Unity3D.sh

---
+++ Installl-Unity3D.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+	echo "Error: This script requires root privileges. Please run with sudo."
+	exit 1
+fi
+
+# Is wget installed?
+dpkg -s wget &> /dev/null
+if [ $? -eq 0 ]; then
+	echo -e "wget is installed! Dependencies complete!"
+else
+	echo "The package wget is NOT installed! Wegt will be installed in 5 seconds! (Press Ctrl + C to cancel)"
+	sleep 5
+	apt install wget
+fi
+
+# Is steam installed?
+dpkg -s unityhub &> /dev/null
+if [ $? -eq 0 ]; then
+	echo -e "Unity is already installed."
+else
+	wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+	sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+	apt update
+	apt install unityhub
+fi
+
+exit
