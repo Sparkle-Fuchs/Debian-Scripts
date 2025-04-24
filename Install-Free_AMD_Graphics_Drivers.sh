c875d2aa326f5b9432f11d2f0641c2619f7671f3
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-24 19:54:40 +0200

    Update Install-Free_AMD_Graphics_Drivers.sh

--- Install-Free_AMD_Graphics_Drivers.sh
+++ Install-Free_AMD_Graphics_Drivers.sh
@@ -21,6 +21,7 @@ if [ $? -eq 0 ]; then
 	dpkg --add-architecture i386 && apt update
 	apt install libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 	apt install lm-sensors radeontop
+ 	sudo init 6
 fi
 
 exit
