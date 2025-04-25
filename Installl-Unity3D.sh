e1f9019d4fdf51a9dd7f5b239c54b2d6232935f3
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-25 12:25:35 +0200

    Update Installl-Unity3D.sh

--- Installl-Unity3D.sh
+++ Installl-Unity3D.sh
@@ -24,7 +24,7 @@ else
 	wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
 	sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 	apt update
-	apt install unityhub
+	apt install unityhub -y
 fi
 
 exit
