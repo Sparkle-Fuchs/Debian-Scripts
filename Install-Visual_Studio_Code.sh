1e4af0194cc0224f498d5b5c66240a8627ced9e0
Sparkle-Fuchs <sparklefuchs@gmail.com>
2025-04-25 12:21:11 +0200

    Update Install-Visual_Studio_Code.sh

--- Install-Visual_Studio_Code.sh
+++ Install-Visual_Studio_Code.sh
@@ -33,7 +33,7 @@ install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packa
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 
-apt update && apt upgrade
+apt update
 apt install code
 
 echo "Visual Studio Code is installed!"
